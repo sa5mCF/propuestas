65512d0829974ce970eff929781280970b32926c
SamChavez <sa5m.escom@gmail.com>
2026-06-04 22:55:28 -0600

    fix: images

--- propuestas.py
+++ propuestas.py
@@ -3,6 +3,28 @@ from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 from playwright.sync_api import sync_playwright
 
+
+def get_image_as_base64(path):
+    with open(path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    return f"data:image/png;base64,{encoded_string}"
+
+
+def images_dict(base_dir: Path):
+
+    return {
+        "historigrama_sti": get_image_as_base64(base_dir / "sti" / "histograma_sti.png"),
+        "spl1": get_image_as_base64(base_dir / "spl" / "spl1.png"),
+        "npx": get_image_as_base64(base_dir / "products" / "npx.jpg"),
+        "alc_1604d": get_image_as_base64(base_dir / "products" / "alc-1604d.png"),
+        "ease0003": get_image_as_base64(base_dir / "sti" / "Ease0003.png"),
+        "spl2": get_image_as_base64(base_dir / "spl" / "spl2.png"),
+        "spl3": get_image_as_base64(base_dir / "spl" / "spl3.png"),
+        "cca_80d": get_image_as_base64(base_dir / "products" / "CCA-80D.png"),
+        "r15_3696": get_image_as_base64(base_dir / "products" / "R15-3696.png")
+    }
+
+
 def render_html():
     base_dir = Path(__file__).parent.absolute()
     env = Environment(loader=FileSystemLoader(base_dir / "templates"))

bf539d0cc51fc2a968867b7758036fd67de310fd
SamChavez <sa5m.escom@gmail.com>
2026-06-08 11:54:46 -0600

    feat: can add other docs and fix juftify content

--- propuestas.py
+++ propuestas.py
@@ -14,36 +14,37 @@ def get_image_as_base64(path):
 def images_dict(base_dir: Path):
 
     return {
-        "historigrama_sti": get_image_as_base64(base_dir / "sti" / "histograma_sti.png"),
-        "spl1": get_image_as_base64(base_dir / "spl" / "spl1.png"),
-        "npx": get_image_as_base64(base_dir / "products" / "npx.jpg"),
-        "alc_1604d": get_image_as_base64(base_dir / "products" / "alc-1604d.png"),
-        "ease0003": get_image_as_base64(base_dir / "sti" / "Ease0003.png"),
-        "spl2": get_image_as_base64(base_dir / "spl" / "spl2.png"),
-        "spl3": get_image_as_base64(base_dir / "spl" / "spl3.png"),
-        "cca_80d": get_image_as_base64(base_dir / "products" / "CCA-80D.png"),
-        "r15_3696": get_image_as_base64(base_dir / "products" / "R15-3696.png"),
-        "slxd": get_image_as_base64(base_dir / "products" / "slxd.png")
+        "propuesta1": {
+            "historigrama_sti": get_image_as_base64(base_dir / "propuesta1/sti" / "histograma_sti.png"),
+            "spl1": get_image_as_base64(base_dir / "propuesta1/spl" / "spl1.png"),
+            "npx": get_image_as_base64(base_dir / "propuesta1/products" / "npx.jpg"),
+            "alc_1604d": get_image_as_base64(base_dir / "propuesta1/products" / "alc-1604d.png"),
+            "ease0003": get_image_as_base64(base_dir / "propuesta1/sti" / "Ease0003.png"),
+            "spl2": get_image_as_base64(base_dir / "propuesta1/spl" / "spl2.png"),
+            "spl3": get_image_as_base64(base_dir / "propuesta1/spl" / "spl3.png"),
+            "cca_80d": get_image_as_base64(base_dir / "propuesta1/products" / "CCA-80D.png"),
+            "r15_3696": get_image_as_base64(base_dir / "propuesta1/products" / "R15-3696.png"),
+            "slxd": get_image_as_base64(base_dir / "propuesta1/products" / "slxd.png"),
+        }
     }
 
 
-def render_html():
+def render_html(template_name: str, images_assets_name: str):
     base_dir = Path(__file__).parent.absolute()
     env = Environment(loader=FileSystemLoader(base_dir / "templates"))
-    template = env.get_template("proposal.html")
+    template = env.get_template(template_name)
     
     base_dir = Path(__file__).parent.absolute()
     assets_dir = base_dir / "assets"
 
     return template.render(
-            images=images_dict(base_dir / "assets")
+            images=images_dict(base_dir / "assets")[images_assets_name]
         )
 
 
-def main():
+def generate_pdf(template_name: str, images_assets_name: str, out_file: str):
     base_dir = Path(__file__).parent.absolute()
-    html_content = render_html()
-    
+    html_content = render_html(template_name, images_assets_name)
         
     print("Generating PDF via Playwright...")
     with sync_playwright() as p:
@@ -56,7 +57,7 @@ def main():
         css_path = base_dir / "static" / "styles.css"
         page.add_style_tag(path=str(css_path))
 
-        pdf_path = base_dir / "output" / "proposal.pdf"
+        pdf_path = base_dir / "output" / out_file
         page.pdf(
             path=str(pdf_path),
             format="A4",
@@ -71,6 +72,10 @@ def main():
         browser.close()
         
     print(f"PDF successfully generated: {pdf_path}")
+    
+
+def main():
+    generate_pdf("propuesta1/proposal.html", "propuesta1", "proposal.pdf")
 
 if __name__ == "__main__":
     main()
